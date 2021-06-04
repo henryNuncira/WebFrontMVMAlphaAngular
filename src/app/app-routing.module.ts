c38ebc46c566e836f8123daa20a9c55c967883da
henryNuncira <inghenrynuncira@gmail.com>
2021-06-04 13:08:52 -0500

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContAdminComponent } from './shared/components/cont-admin/cont-admin.component';
 import { DashboardComponent } from './shared/components/dashboard/dashboard.component';
 import { LoginComponent } from './shared/components/login/login.component';
+import { GuardGuard } from './shared/gurds/guard.guard';
 
 const routes: Routes = [
 
@@ -10,12 +11,12 @@ const routes: Routes = [
   {path: 'dashboard', component:DashboardComponent},
 
   {path: 'login', component:LoginComponent},
-  {path: 'contAdmin', component:ContAdminComponent},
+  {path: 'contAdmin', component:ContAdminComponent, canActivate:[GuardGuard]},
    { path: 'listadoUsuarios', loadChildren: () => import('./pages/usuarios/listado-usuarios/listado-usuarios.module').then(m => m.ListadoUsuariosModule) },
    { path: 'nuevoUsuarios', loadChildren: () => import('./pages/usuarios/nuevo-usuarios/nuevo-usuarios.module').then(m => m.NuevoUsuariosModule) },
    { path: 'modificarUsuarios', loadChildren: () => import('./pages/usuarios/modificar-usuarios/modificar-usuarios.module').then(m => m.ModificarUsuariosModule) },
    { path: 'modificarRadicado', loadChildren: () => import('./pages/radicado/modificar-radicado/modificar-radicado.module').then(m => m.ModificarRadicadoModule) },
-   { path: 'nuevoRadicado', loadChildren: () => import('./pages/radicado/nuevo-radicado/nuevo-radicado.module').then(m => m.NuevoRadicadoModule) },
+   { path: 'nuevoRadicado', loadChildren: () => import('./pages/radicado/nuevo-radicado/nuevo-radicado.module').then(m => m.NuevoRadicadoModule),canActivate:[GuardGuard] },
    { path: 'listaRadicado', loadChildren: () => import('./pages/radicado/listado-radicado/listado-radicado.module').then(m => m.ListadoRadicadoModule) },
    { path: 'listaPersona', loadChildren: () => import('./pages/personaContacto/listado-persona/listado-persona.module').then(m => m.ListadoPersonaModule) },
    { path: 'nuevoPersona', loadChildren: () => import('./pages/personaContacto/nuevo-persona/nuevo-persona.module').then(m => m.NuevoPersonaModule) },
